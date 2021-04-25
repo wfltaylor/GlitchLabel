51ec3539687c525e3aa6a41a6f4a231b8b180b18
William Taylor <wfltaylor@gmail.com>
2021-04-25 14:12:48 +1000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,3 +1,18 @@
+// swift-tools-version:5.3
+
 import PackageDescription
 
-let package = Package(name: "GlitchLabel")
+let package = Package(
+    name: "GlitchLabel",
+    platforms: [.iOS(.v9)],
+    products: [
+        .library(
+            name: "GlitchLabel",
+            targets: ["GlitchLabel"]),
+    ],
+    targets: [
+        .target(
+            name: "GlitchLabel",
+            path: "Sources"),
+    ]
+)
